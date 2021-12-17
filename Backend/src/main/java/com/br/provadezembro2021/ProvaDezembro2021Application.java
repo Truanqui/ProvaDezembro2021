0bf071f9006e65b6ee58932e736d90a45f8145f8
Johann <johannteixeira2@gmail.com>
2021-12-16 23:41:47 -0300

    Backend em adamento

---
+++ Backend/src/main/java/com/br/provadezembro2021/ProvaDezembro2021Application.java
@@ -0,0 +1,13 @@
+package com.br.provadezembro2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaDezembro2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaDezembro2021Application.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/br/provadezembro2021/ProvaDezembro2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.provadezembro2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaDezembro2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
